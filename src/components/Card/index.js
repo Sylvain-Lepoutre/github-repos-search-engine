0b2d9d96e61c96eb46c79c0df99bcb07b192706d
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-06-11 13:14:14 +0200

    mixin/media query

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,25 @@
 import './style.scss';
 
 function Card( { name, img, desc, link} ) {
+
+    function truncateString(string) {
+        if (string === null) {
+          return '';
+        }
+        else if(string.length > 50) {
+          return string.slice(0, 50) + "...";
+        }
+        else {
+          return string;
+        }
+      }
+
+
     return (
         <article className="card">
             <h3 className="card__title">{name}</h3>
             <img className="card__img" src={img} alt="{name}" />
-            <p className="card__desc">{desc}</p>
+            <p className="card__desc">{truncateString(desc)}</p>
             <a className="card__link" href={link}>Aller sur ce repos</a>
         </article>
     )
